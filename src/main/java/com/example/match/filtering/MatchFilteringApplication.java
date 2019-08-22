d2cc1f0c444341ba089ef831c7c0e15d2cdaab93
Gopal Mondal <gopalm700@gmail.com>
2019-08-22 21:44:13 +0200

    Adding Project Base

---
+++ src/main/java/com/example/match/filtering/MatchFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.example.match.filtering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchFilteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/match/filtering/MatchFilteringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.match.filtering;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatchFilteringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
